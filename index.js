3e3179ae25d3cb8cc46f8acc15c95d69e07e23a0
riskiputraalamzah <riskialamzah1@gmail.com>
2025-05-13 21:32:24 +0700

    add vercel.json

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Welcome to the User API");
+});
 app.use("/users", userRoutes);
 
 app.listen(port, () =>
